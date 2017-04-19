2bc92ca42d81d58b5f119fcb91290c539119c83b
mohesan <mohe_2322@hotmail.com>
2017-04-19 17:09:59 -0400

    class creation

---
+++ E-M-Class.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class EMsim:
+    """ This Class is used to simulate the motion of particles with charges under
+    the influence of an electric field
+    Attributes:
+        
+    Methods:
+        
+        
+    """
+    
+    def __init__(self, temp=3.,size=10):
+        """Sets up the simulation
+
+        Args:
+        
+        """
+        
+        
+        
