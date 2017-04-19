156b0c0e320a9a13e3651ea4dd2db8cd58af3a3b
Mladen Kolovic <mkolovic@uwaterloo.ca>
2017-04-19 18:06:09 -0400

    Added method skeletons

--- E-M-Class.py
+++ E-M-Class.py
@@ -4,18 +4,31 @@ class EMsim:
     """ This Class is used to simulate the motion of particles with charges under
     the influence of an electric field
     Attributes:
-        
+
     Methods:
-        
-        
+
+
     """
-    
-    def __init__(self, temp=3.,size=10):
+
+    def __init__(self, phase_space, t_data = (0, 1, 0.1), boundary=False, fields = False):
         """Sets up the simulation
 
         Args:
-        
+
         """
-        
-        
-        
+
+        self.phase_space = phase_space
+        self.t_start, self.t_end, self.t_step_base = t_data
+        self.boundary = boundary
+
+    def a_e_field(self):
+        return 0
+
+    def a_e_particle(self):
+        return 0
+
+    def max_v(self):
+        return 0
+
+    def collisions(self):
+        return 0
